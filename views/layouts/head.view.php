22f0848482ba9dee0aba3e6f47ecf18e7a1ca6ba
Erkan <erkan.aslantas@windesheim.nl>
2024-10-19 02:26:45 +0200

    head update

--- views/layouts/head.view.php
+++ views/layouts/head.view.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/views/css/style.css">
+    <meta name="author" content="Erkan Aslantas">\
+    <meta name="keywords" content="Portfolio">
+    <meta name="robots" content="robots follow">
     <title><?php echo htmlspecialchars($title); ?></title>
 </head>
 <body>
